b1f99827dd565ed6f2891399dd1a81a2101ea979
MusaBellur <114991207+MusaBellur@users.noreply.github.com>
2023-07-29 23:36:29 +0300

    Added Entity Layers And Edited Navbar

---
+++ EntityLayer/EntityAppForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    internal class EntityAppForm
+    {
+        private int appId;
+        private int appStuId;
+        private int appLesId;
+
+        public int AppId { get => appId; set => appId = value; }
+        public int AppStuId { get => appStuId; set => appStuId = value; }
+        public int AppLesId { get => appLesId; set => appLesId = value; }
+    }
+}
---
+++ EntityLayer/EntityStudents.cs
@@ -0,0 +1,42 @@
+﻿namespace EntityLayer
+{
+    public class EntityStudents
+    {
+        private int stuId;
+        private string stuName;
+        private string lastname;
+        private string number;
+        private string picture;
+        private decimal balance;
+
+        public int StuId 
+        { 
+            get => stuId; set => stuId = value; 
+        }
+        public string StuName 
+        { 
+            get => stuName; set => stuName = value; 
+        }
+        public string Lastname
+        { 
+            get => lastname; 
+            set => lastname = value; 
+        }
+        public string Number 
+        { 
+            get => number; 
+            set => number = value; 
+        }
+        public string Picture 
+        { 
+            get => picture; 
+            set => picture = value; 
+        }
+        public decimal Balance 
+        { 
+            get => balance; 
+            set => balance = value; 
+        }
+
+    }
+}
